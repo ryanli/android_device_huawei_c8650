9ce49afc086aa04d2eace15ea9063b3594fe45d5
Ryan Li <ryan@ryanium.com>
2011-11-27 23:27:16 +0800

    Initial commit.

---
+++ BoardConfig.mk
@@ -0,0 +1,140 @@
+# Copyright (C) 2011 Ryan Li <ryan@ryanium.com>
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from the proprietary version
+-include vendor/huawei/c8650/BoardConfigVendor.mk
+
+# CPU
+# MSM7627, ARM1136EJ-S
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_ARCH_VARIANT := armv5te-vfp
+TARGET_CPU_ABI := armeabi
+TARGET_GLOBAL_CFLAGS += -mfpu=vfp -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=vfp -mfloat-abi=softfp
+
+# Target properties
+TARGET_BOOTLOADER_BOARD_NAME := c8650
+TARGET_OTA_ASSERT_DEVICE := c8650,C8650
+
+# Target information
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RECOVERY := false
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+
+# Touchscreen
+TARGET_HAVE_TSLIB := true
+
+# Camera
+BOARD_CAMERA_LIBRARIES := libcamera
+
+# GPS
+# disabled temporarily due to link error
+# BOARD_USES_QCOM_GPS := true
+# BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
+# BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := c8650
+
+# Java VM
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+
+# JavaScript Engine
+JS_ENGINE := v8
+
+# USB mass storage
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_CUSTOM_USB_CONTROLLER := ../../device/huawei/c8650/UsbController.cpp
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+
+# Sensors
+TARGET_USES_OLD_LIBSENSORS_HAL := true
+BOARD_VENDOR_USE_AKMD := akm8973
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/huawei/c8650/recovery/recovery_ui.c
+BOARD_LDPI_RECOVERY := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+TARGET_PROVIDES_LIBAUDIO := true
+
+# RIL
+TARGET_PROVIDES_LIBRIL := true
+
+# Graphics
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+BOARD_USES_ADRENO_200 := true
+HAVE_ADRENO200_SOURCE := true
+HAVE_ADRENO200_SC_SOURCE := true
+HAVE_ADRENO200_FIRMWARE := true
+TARGET_HARDWARE_3D := false
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_SF_NEEDS_REAL_DIMENSIONS := true
+BOARD_NO_RGBX_8888 := true
+BOARD_EGL_CFG := device/huawei/c8650/egl.cfg
+BOARD_HAS_LIMITED_EGL := true
+TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
+
+# WiFi
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+WIFI_DRIVER_MODULE_PATH := "/system/wifi/dhd.ko"
+WIFI_DRIVER_FW_STA_PATH := "/system/wifi/firmware.bin"
+WIFI_DRIVER_FW_AP_PATH := "/system/wifi/firmware_apsta.bin"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/wifi/firmware.bin nvram_path=/system/wifi/nvram.txt"
+
+# Kernel
+BOARD_KERNEL_BASE := 0x10200000
+BOARD_KERNEL_CMDLINE := console=ttyDCC0 androidboot.hardware=huawei
+BOARD_KERNEL_PAGESIZE := 2048
+
+# # cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 00500000 00020000 "boot"
+# mtd1: 00500000 00020000 "recovery"
+# mtd2: 00140000 00020000 "misc"
+# mtd3: 00060000 00020000 "splash"
+# mtd4: 0ba00000 00020000 "system"
+# mtd5: 03f00000 00020000 "cache"
+# mtd6: 0a0a0000 00020000 "userdata"
+# mtd7: 01400000 00020000 "userdata2"
+# mtd8: 01400000 00020000 "cust"
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0ba00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0a0a0000
+
+# (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_FLASH_BLOCK_SIZE := 131072
---
+++ device_c8650.mk
@@ -0,0 +1,100 @@
+# Copyright (C) 2011 Ryan Li <ryan@ryanium.com>
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for passion hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+$(call inherit-product-if-exists, vendor/huawei/c8650/c8650-vendor-blobs.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+$(call inherit-product, device/common/gps/gps_as_supl.mk)
+
+$(call inherit-product-if-exists, device/huawei/c8650/wifi/wifi.mk)
+
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni \
+    Gallery3d \
+    SpareParts \
+    Term \
+    libcamera \
+    libOmxCore \
+    libOmxVidEnc \
+    dexpreopt \
+    gps.c8650 \
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/c8650/overlay
+
+# Vold config
+PRODUCT_COPY_FILES += \
+    device/huawei/c8650/prebuilt/etc/vold.fstab:system/etc/vold.fstab
+
+# Hardware permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Init files
+PRODUCT_COPY_FILES += \
+	device/huawei/c8650/prebuilt/init:root/init \
+    device/huawei/c8650/prebuilt/init.rc:root/init.rc \
+    device/huawei/c8650/prebuilt/init.highmem.rc:root/init.highmem.rc \
+    device/huawei/c8650/prebuilt/init.huawei.rc:root/init.huawei.rc \
+    device/huawei/c8650/prebuilt/init.mem.rc:root/init.mem.rc \
+    device/huawei/c8650/prebuilt/ueventd.rc:root/ueventd.rc
+
+# Kernel
+PRODUCT_COPY_FILES += \
+	device/huawei/c8650/prebuilt/kernel:kernel
+
+# OEM logo and boot splash
+PRODUCT_COPY_FILES += \
+    device/huawei/c8650/prebuilt/media/oemlogo.mbn:system/media/oemlogo.mbn
+
+# Wi-Fi releated
+PRODUCT_COPY_FILES += \
+    device/huawei/c8650/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/huawei/c8650/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# Other
+PRODUCT_COPY_FILES += \
+    device/huawei/c8650/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv
+
+# LDPI assets
+PRODUCT_LOCALES += ldpi mdpi
+
+PRODUCT_NAME := full_c8650
+PRODUCT_DEVICE := c8650
+PRODUCT_MODEL := C8650
+PRODUCT_BRAND := huawei
+PRODUCT_MANUFACTURER := huawei
