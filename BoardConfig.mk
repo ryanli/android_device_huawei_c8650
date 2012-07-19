996a3aeeb256d4d381d3e3fbd7fa79f8a95f86ea
Ryan Li <shli@redhat.com>
2012-07-19 15:36:02 +0800

    Use default BOARD_UMS_LUNFILE.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,6 @@ JS_ENGINE := v8
 # USB mass storage
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun
 
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/huawei/c8650/recovery/recovery_ui.c
